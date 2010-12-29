cf3c6d3ed467d47b762af42f9f62231279a9a0d6
Philipp Sieweck <psieweck@freenet.de>
2010-12-29 18:55:19 +0100

    initial commit

---
+++ edu/mit/csail/sdg/alloy4/Version.java
@@ -0,0 +1,7 @@
+package edu.mit.csail.sdg.alloy4;
+public final class Version {
+private Version() {}
+public static final boolean experimental = false;
+public static int buildNumber() { return 905; }
+public static String version() { return "4.1.10"; }
+public static String buildDate() { return "2009/03/19 02:02 EDT"; }}
